557f7753cdee2c7336c36ebb4e6d0c1bcd3179bd
Ajay Maharjan <ajaymrzn02@gmail.com>
2021-08-31 12:32:18 +0545

    Add votes, answers and views counter to question item

---
+++ .history/app/Models/Question_20210831122617.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title','body'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] =  Str::slug($value);
+    }
+
+    public function gerUrlAttribute(){
+        return route("question.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute(){
+        if ($this->answer > 0){
+            if {$this->best_answer_id}{
+                return "answered-accepted";
+            }
+            re
+        }
+    }
+}
---
+++ .history/resources/views/question/index.blade_20210831112511.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @foreach ($question as $q )
+                        <div class="media">
+                            div.d-flex flex-col
+                            <div class="media-body">
+                                <h3 class="mt-0"> <a href="{{  $q->url  }}}}">{{ $q->title }}</a></h3>
+                                <p class="lead">
+                                    Asked by
+                                    <a href="{{ $q->user->url }}">{{ $q->user->name }}</a>
+                                    <small class="text-muted">{{ $q->created_date }}</small>
+                                </p>
+                                {{ Str::limit($q->body, 250) }}
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="d-flex justify-content-center">
+                        {{ $question->links() }}
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
